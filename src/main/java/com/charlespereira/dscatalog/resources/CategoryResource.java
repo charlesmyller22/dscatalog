23a860a986514ac7e75da3d000db3445f80aebcf
Charles Pereira <charlesmyller@hotmail.com>
2023-06-19 15:15:56 +0200

    DTO implemented

--- src/main/java/com/charlespereira/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/charlespereira/dscatalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.charlespereira.dscatalog.resources;
 
+import com.charlespereira.dscatalog.dto.CategoryDTO;
 import com.charlespereira.dscatalog.entities.Category;
 import com.charlespereira.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +18,8 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/charlespereira/dscatalog/services/CategoryService.java
+++ src/main/java/com/charlespereira/dscatalog/services/CategoryService.java
@@ -1,17 +1,22 @@
 package com.charlespereira.dscatalog.services;
 
+import com.charlespereira.dscatalog.dto.CategoryDTO;
 import com.charlespereira.dscatalog.entities.Category;
 import com.charlespereira.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepository repository;
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
     }
 }
