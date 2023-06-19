e6f34ef3ad3837520965e31511c481b71173b97d
Charles Pereira <charlesmyller@hotmail.com>
2023-06-19 08:42:57 +0200

    Project created

---
+++ src/main/java/com/charlespereira/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.charlespereira.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
