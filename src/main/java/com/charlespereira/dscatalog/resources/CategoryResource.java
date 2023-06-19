91b89d03d516f031638dff6e03956807944710a8
Charles Pereira <charlesmyller@hotmail.com>
2023-06-19 12:48:35 +0200

    h2/PostgreSql database, test and dev profile created

---
+++ src/main/java/com/charlespereira/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,24 @@
+package com.charlespereira.dscatalog.resources;
+
+import com.charlespereira.dscatalog.entities.Category;
+import com.charlespereira.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+    @Autowired
+    private CategoryService service;
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
